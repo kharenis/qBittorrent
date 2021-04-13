bfe8be1413ea912718bce3a3098142aa9671c631
Richard <richard.smothers@hotmail.co.uk>
2021-04-13 19:42:28 +0100

    Fixed typo

--- src/webui/webapplication.cpp
+++ src/webui/webapplication.cpp
@@ -499,7 +499,7 @@ Http::Response WebApplication::processRequest(const Http::Request &request, cons
 
         //Replace base path prefix with a '/'
         if(!m_basePath.isEmpty() && m_request.path.indexOf(m_basePath == 0))
-            m_request.path.replace(0, m_basePath.length, QChar('/'));
+            m_request.path.replace(0, m_basePath.length(), QChar('/'));
 
         doProcessRequest();
     }
