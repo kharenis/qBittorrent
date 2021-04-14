60ee7ff0a0119805223efabcfb0e8d139f5b89c2
Richard <richard.smothers@hotmail.co.uk>
2021-04-14 10:23:59 +0100

    Changed header setting

--- src/webui/webapplication.cpp
+++ src/webui/webapplication.cpp
@@ -267,8 +267,8 @@ void WebApplication::doProcessPath()
         }
         else
         {
-        //Set location header for redirect
-            m_response.headers[Http::HEADER_LOCATION] = m_basePath + m_request.path;
+            //Set location header for redirect
+            setHeader({Http::HEADER_LOCATION, m_basePath + m_request.path});
             throw SeeOtherHTTPError(m_basePath + m_request.path);
         }
     }
